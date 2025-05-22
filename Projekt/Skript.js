8bf328d4f938bda2cb257a961e5d2162f9597cd5
MeoBunnfors <meobun479@edu.linkoping.se>
2025-05-22 15:55:07 +0000

    end

--- Projekt/Skript.js
+++ Projekt/Skript.js
@@ -6,11 +6,8 @@ function toggleCheck(listItem) {
 
       const hamburger = document.querySelector(".hamburger");
         const linksMenu = document.querySelector(".links-menu");
-
-        // Add event listener to the hamburger icon
+        
         hamburger.addEventListener("click", () => {
-            // Toggle the 'change' class on the hamburger for the 'X' animation
             hamburger.classList.toggle("change");
-            // Toggle the 'active' class on the navigation links to show/hide the menu
             linksMenu.classList.toggle("active");
         });
